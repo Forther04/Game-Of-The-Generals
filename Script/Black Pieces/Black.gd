ebabdb664ff4a5d760e145d7073d82061316fd67
H20M <clarkpogi865@gmail.com>
2025-02-10 22:50:39 +0800

    Working selecting and deselecting

--- Script/Black Pieces/Black.gd
+++ Script/Black Pieces/Black.gd
@@ -2,8 +2,13 @@ extends Sprite2D
 
 
 @export var pieces : piece
+var off : bool = false
 enum piece {Flag,General_5,General_4,General_3,General_2,General_1}
 
+func _process(delta: float) -> void:
+	if Global.piece != $".".name:
+		$Choices.hide()
+		off = false
 
 func change_piece():
 	if pieces == 0:
@@ -24,3 +29,12 @@ func change_piece():
 	if pieces == 5:
 		self.region_rect = Rect2(170,0,32,32)
 		self.name = "1_General"
+
+func _on_main_pressed() -> void:
+	Global.piece = $".".name
+	if off == false:
+		$Choices.show()
+		off = true
+	elif off == true:
+		$Choices.hide()
+		off = false

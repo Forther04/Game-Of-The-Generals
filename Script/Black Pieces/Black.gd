58a4355b421f06b7b5fa7aa15f2c84c7b8e3ba8a
H20M <clarkpogi865@gmail.com>
2025-02-11 23:01:22 +0800

    Revert "Fully functional piece selector"
    This reverts commit 1ed92239efb8e7f4ff064d6b591d1fb9fb0c9119.

--- Script/Black Pieces/Black.gd
+++ Script/Black Pieces/Black.gd
@@ -2,28 +2,17 @@ extends Sprite2D
 
 
 @export var pieces : piece
-@export var right_area : Area2D
-@export var left_area : Area2D
-@export var top_area : Area2D
-@export var bottom_area : Area2D
 var off : bool = false
 var black_piece_area
-var check_timer = 0.0
-@export var check_interval = 0.2
-
 enum piece {Flag,General_5,General_4,General_3,General_2,General_1}
 
 
 func _process(delta: float) -> void:
 	change_piece()
-	check_timer += delta
+	overlapping()
 	if Global.piece != $".".name: #Check if a new piece is selected and hide the previous one
 		$Choices.hide()
 		off = false
-	if check_timer >= check_interval:
-		check_timer = 0
-		overlapping()
-
 
 func change_piece(): #Changes the piece type
 	if pieces == 0:
@@ -57,48 +46,23 @@ func _on_main_pressed() -> void:
 		off = false
 
 func overlapping():
-	var right = right_area.get_overlapping_areas()
-	var left = left_area.get_overlapping_areas()
-	var top = top_area.get_overlapping_areas()
-	var bottom = bottom_area.get_overlapping_areas()
-
 	var black = $Black
 	if $Choices/Top/TOp.overlaps_area($"../Border/AreaBorder"):
 		$Choices/Top.hide()
 	else:
 		$Choices/Top.show()
-	if bottom_area.overlaps_area($"../Border/AreaBorder"):
+	if $Choices/Bottom/BOttom.overlaps_area($"../Border/AreaBorder"):
 		$Choices/Bottom.hide()
 	else:
 		$Choices/Bottom.show()
-	if left_area.overlaps_area($"../Border/AreaBorder"):
+	if $Choices/Left/Left.overlaps_area($"../Border/AreaBorder"):
 		$Choices/Left.hide()
 	else:
 		$Choices/Left.show()
-	if right_area.overlaps_area($"../Border/AreaBorder"):
+	if $Choices/Right/Right.overlaps_area($"../Border/AreaBorder"):
 		$Choices/Right.hide()
 	else:
 		$Choices/Right.show()
-		
-	for area in right:
-		if area.is_in_group("Black_Area"):  
-			if right_area.overlaps_area(area):
-				$Choices/Right.hide()
-	for area in left:
-		if area.is_in_group("Black_Area"):  
-			if left_area.overlaps_area(area):
-				$Choices/Left.hide()
-	for area in top:
-		if area.is_in_group("Black_Area"):  
-			if top_area.overlaps_area(area):
-				$Choices/Top.hide()
-	for area in bottom:
-		if area.is_in_group("Black_Area"):  
-			if bottom_area.overlaps_area(area):
-				$Choices/Bottom.hide()
-
-
-
 
 
 func _on_top_pressed() -> void:
